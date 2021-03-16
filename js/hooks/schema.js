158a39c3b8c9e2b4334f22f2d2068ad0cb075000
Vinson Xing <vixing@cisco.com>
2021-03-16 22:23:08 +0800

    revert

--- js/hooks/schema.js
+++ js/hooks/schema.js
@@ -2,58 +2,49 @@
 export const houseListSchemas = [
   {
     field: 'title',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .title a',
+    selector: '.sellListContent li.LOGCLICKDATA .title a',
     type: 'String',
   },
   {
     field: 'img',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA a.img img.lj-lazy',
+    selector: '.sellListContent li.LOGCLICKDATA a.img img.lj-lazy',
     type: 'String',
     attribute: 'data-original',
   },
   {
     field: 'detailLink',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .title a',
+    selector: '.sellListContent li.LOGCLICKDATA .title a',
     type: 'String',
     attribute: 'href',
   },
   {
     field: 'positionInfo',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .positionInfo',
+    selector: '.sellListContent li.LOGCLICKDATA .positionInfo',
     type: 'String',
   },
   {
     field: 'houseInfo',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .houseInfo',
+    selector: '.sellListContent li.LOGCLICKDATA .houseInfo',
     type: 'String',
   },
   {
     field: 'followInfo',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .followInfo',
+    selector: '.sellListContent li.LOGCLICKDATA .followInfo',
     type: 'String',
   },
   {
     field: 'priceInfo',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .priceInfo .totalPrice',
+    selector: '.sellListContent li.LOGCLICKDATA .priceInfo .totalPrice',
     type: 'String',
   },
   {
     field: 'unitPrice',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .priceInfo .unitPrice',
+    selector: '.sellListContent li.LOGCLICKDATA .priceInfo .unitPrice',
     type: 'String',
   },
   {
     field: 'tag',
-    selector:
-      '.sellListContent[data-lj_action_type="list_zaishou"] li.LOGCLICKDATA .tag',
+    selector: '.sellListContent li.LOGCLICKDATA .tag',
     type: 'StringOfArray',
     moreSelector: 'span',
     separator: ' ',
